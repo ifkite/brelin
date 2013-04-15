61f61267c46072a4ca4c8c8a484eb33a6cd5e90a
ifkite <if.kite@gmail.com>
2013-04-15 10:52:57 +0800

    test input

---
+++ compiler/src/Yacc/test/get.c
@@ -0,0 +1,21 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	FILE * fp;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	fp = fopen("./getme", "r");
+	if (fp == NULL)
+		 exit(EXIT_FAILURE);
+	while ((read = getline(&line, &len, fp)) != -1) {
+		 printf("Retrieved line of length %zu :\n", read);
+		 printf("%s", line);
+	}
+	if (line)
+		 free(line);
+	return 0;
+}
--- compiler/src/Yacc/test/initRul.cpp
+++ compiler/src/Yacc/test/initRul.cpp
@@ -6,13 +6,13 @@ using std::vector;
 typedef struct Id{
 	int a;
 	char ch;
-	vector<char> str;
+	//vector<char> str;
 }Id;
 
 int main(){
-	Id id1 = {id1.a = 1, id1.ch = 'a'};
+	Id id1 = {id1.a = 1};
 	Id id2 = {id2.a = id1.a, id2.ch = id1.ch};
-	id2.str.push_back('s');
-	printf("%d:%c\n", id2.a, id2.ch);
+	//id2.str.push_back('s');
+	printf("%d:%c%c\n", id2.a, id1.ch);
 	return 0;
 }
