f3ac7d45016b37d70be08a9f78c9f62fd6e9f600
ifkite <if.kite@gmail.com>
2013-04-26 21:03:06 +0800

    read conitional tab form file

--- compiler/src/Tex/Tex.cpp
+++ compiler/src/Tex/Tex.cpp
@@ -3,17 +3,48 @@
 #include<stdio.h>
 #include<stdlib.h>
 using std::stack;
-/*
-void init_input(int *buf, const int size){
-	int i;
-	for(i = 0; i < size; ++i)
-		buf[i] = 0;
-}
-*/
+
 //need to be modified
+//read action tab and goto tab from file
+//modify act_tab in this func
+static int readTab(){
+	FILE* file;
+	file = fopen(tab_path , "r");
+	if(!file){
+		perror("error open file\n");
+	}
+	else{
+		int loop_i = 0, loop_j = 0, num  = 0;
+		while(fscanf(file, "%d", &num) != EOF){
+			
+			act_tab[loop_j][loop_i] = num;
+			//read character by line
+			++loop_i;
+			if(! (loop_i % ID_SIZ)){
+				loop_i = 0;
+				++loop_j;
+			}
+		}
+
+		//debug cod
+		int loop_col, loop_row;
+		for(loop_col = 0; loop_col < loop_j; ++loop_col){
+			for(loop_row = 0; loop_row < ID_SIZ; ++loop_row)
+				printf("%d\t", act_tab[loop_col][loop_row]);
+			printf("\n");
+		}
+		//debug cod
+		
+	}
+	fclose(file);
+	return 0;
+}
+
 const int BUF_SIZ = 128;
-int input_buf[BUF_SIZ] = {L_BR, ID , ADD, ID, R_BR, MUT, ID, SPC, END};
+//int input_buf[BUF_SIZ] = {L_BR, ID , ADD, ID, R_BR, MUT, ID, SPC, END};
 int main(){
+	readTab();
+	/*
 	int buf;
 	stack<int> stk_stat;
 	stack<int> stk_input;
@@ -22,11 +53,12 @@ int main(){
 	int handle;
 	int action;
 	int stat;
+		
 	if(buf){
 		int next = 1;//next points to the one next to buf
 		int flag = 1;
 		while(flag){
-		
+ 		
 			//readBuf();//read buffer
 			//readStat();//read state in stack
 			//stk_stat.top()	
@@ -35,10 +67,10 @@ int main(){
 			//handle = searchActTab();
 			handle = act_tab[stk_stat.top()][buf];
 			//extract action and state num , which are stored in table
-			/*
-			action = handle & EXTR_ACT; 
-			stat = handle & EXTR_NUM;
-			*/
+			
+			//action = handle & EXTR_ACT; 
+			//stat = handle & EXTR_NUM;
+			
 
 			//in a common way
 			if(handle > 0){
@@ -96,7 +128,7 @@ int main(){
 					flag = 0;
 					break;
 				}
-				default: {
+				 default: {
 					//error
 					fprintf(stderr, "syntax error eccored\n");
 					fprintf(stderr, "saddly, i can not tell you the line now\n");
@@ -106,5 +138,6 @@ int main(){
 			}
 		}	
 	}
+	*/
 	return 0;
 }
--- compiler/src/Tex/Tex.h
+++ compiler/src/Tex/Tex.h
@@ -8,52 +8,68 @@ typedef struct Rule{
 }Rule;
 
 //rule table
-#define RUL_SIZ 7
+#define RUL_SIZ 45
 const Rule rul_tab[RUL_SIZ] = {
-	{"F => id\0",10, 1 },	//0
-	{"F => (E)\0",10, 3 },	//1
-	{"T => F\0", 9, 1},		//2
-	{"T => T * F\0", 9, 3},	//3
-	{"E => T\0", 8, 1},		//4
-	{"E => E + T\0", 8, 3},	//5
-	{"G => E&\0", 7, 2}		//6
+
+	{"Program => main ( ) Compound_stmt ", 30, 4 },
+	{"Compound_stmt => { Declaration_list Statement_list }  " , 31, 4},
+	{"Declaration_list =>  Declaration_list  Declaration " , 32, 2},
+	{"Declaration_list => Declaration " , 32, 1},
+	{"Declaration => Var_declaration " , 39, 1},
+	{"Var_declaration => Type_specifer Identity # " , 40, 3},
+	{"Var_declaration =>  Type_specifer Identity [ ] # " , 40, 5},
+	{"Type_specifer => int " , 41, 1},
+	{"Type_specifer => char " , 41, 1},
+	{"Statement_list => Statement_list Statement " , 33, 1},
+	{"Statement_list => Statement " , 33, 1},
+	{"Statement => Expression_stmt " , 34, 1},
+	{"Statement => If_stmt " , 34, 1},
+	{"Statement => While_stmt " , 34, 1},
+	{"Expression_stmt => Expression # ", 35, 2},
+	{"Expression_stmt => # ", 35, 1},
+	{"If_stmt => if ( Expression ) { Statement } ", 36, 7 },
+	{"If_stmt => if ( Expression ) { Statement } else { Statement } ", 36, 11},
+	{"While_stmt => while ( Expression ) { Statement } ", 37, 7},
+	{"Expression => Var = Expression ", 38, 3},
+	{"Expression => Simple_expression ", 38, 1},
+	{"Expression => Simple_expression and Simple_expression ", 38, 3},
+	{"Expression => Simple_expression or Simple_expression ", 38, 3},
+	{"Expression => not Simple_expression  ", 38, 2},
+	{"Var => Identity ", 42, 1},
+	{"Var => Identity [ Expression ] ", 42, 4},
+	{"Simple_expression => Additive_expression Relop Additive_expression ", 43, 3 },
+	{"Simple_expression => Additive_expression ", 43, 1},
+	{"Relop => <= ", 45, 1},
+	{"Relop => < ", 45, 1},
+	{"Relop => == ", 45, 1},
+	{"Relop => > ", 45, 1},
+	{"Relop => >= ", 45, 1},
+	{"Relop => != ", 45, 1},
+	{"Additive_expression => Additive_expression Addop Term ", 44, 3},
+	{"Additive_expression  => Term ", 44, 1},
+	{"Addop => + ", 46, 1},
+	{"Addop => - ", 46, 1},
+	{"Term => Term Mulop Factor ", 47, 3},
+	{"Term => Factor ", 47, 1},
+	{"Mulop => * ", 48, 1},
+	{"Mulop => / ", 48, 1},
+	{"Mulop => % ", 48, 1},
+	{"Factor => ( Expression ) ", 49, 3},
+	{"Factor=> Var ", 49, 1}
 };
 
 #define SHIFT 0x1000
 #define REDUC 0x2000
 #define ACCPT 0x4000
 #define ERRTX 0x0000
-const int STAT_SIZ = 24;
-const int ID_SIZ = 11;
+//need modify
+const int STAT_SIZ = 310;
+const int ID_SIZ = 50;
+const char *tab_path = "./tab_first.txt\0";
 
-const int act_tab[STAT_SIZ][ID_SIZ] = {
-//  #	  , id	  , +	 , *	, (		,)	  , &	 , G 
-	{0	  , 5,0	 , 0	,6,0	  ,0	 ,1,2,3,4},
-	{ACCPT, 0	  ,0	 , 0	,0     ,0	  ,0	 ,0,0,0,0},
-	{0	  , 0	  ,7, 0	,0	   ,0	  ,8,0,0,0,0},
-	{0	  , 0	  ,-4,9,0	   ,0     ,-4,0,0,0,0},
-	{0	  , 0	  ,-2,-2,0	   ,0     ,-2,0,0,0,0},
-	{0	  , 0	  ,-0,-0,0	   ,0	  ,-0,0,0,0,0},
-	{0	  , 13,0	 ,0		,13,0	  ,0	 ,0,10,11,12},
-	{0	  , 5,0	 ,0		,6,0	  ,0	 ,0,0,15,4},
-	{-6, 0	  ,0	 ,0		,0	   ,0	  ,0	 ,0,0,0,0},
-	{0	  , 5,0	 ,0		,6,0	  ,0	 ,0,0,0,16},
-	{0	  , 0	  ,17,0		,0	   ,18,0	 ,0,0,0,0},
-	{0	  , 0	  ,-4,19,0	   ,-4,0	 ,0,0,0,0},
-	{0	  , 0	  ,-2,-2,0	   ,-2,0	 ,0,0,0,0},
-	{0	  , 0	  ,-0,-0,0	   ,-0,0	 ,0,0,0,0},
-	{0	  , 13,0	 ,0		,14,0	  ,0	 ,0,20,11,12},
-	{0	  ,0	  ,-5,9,0	   ,0	  ,-5,0,0,0,0},
-	{0	  ,0	  ,-3,-3,0	   ,0	  ,-3,0,0,0,0},
-	{0	  ,13 ,0	 ,0		,14,0	  ,0	 ,0,0,21,12},
-	{0	  ,0	  ,-1,-1,0	   ,0	  ,-1,0,0,0,0},
-	{0	  ,13 ,0	 ,0		,14,0	  ,0	 ,0,0,0,22},
-	{0	  ,0	  ,7,0		,0	   ,23,0	 ,0,0,0,0},
-	{0	  ,0	  ,-5,19,0	   ,-5,0	 ,0,0,0,0},
-	{0	  ,0	  ,-3,-3,0	   ,-3,0	 ,0,0,0,0},
-	{0	  ,0	  ,-1,-1,0	   ,-1,0	 ,0,0,0,0}
-};
+int act_tab[STAT_SIZ][ID_SIZ];
 
+/*
 #define END 0
 #define ID  1
 #define ADD 2
@@ -65,6 +81,8 @@ const int act_tab[STAT_SIZ][ID_SIZ] = {
 #define EXP  8
 #define TRM  9
 #define FNL  10
+*/
+
 #define EXTR_ACT 0xf000
 #define EXTR_NUM 0x0fff
 
